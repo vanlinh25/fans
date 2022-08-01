1465f5052a42a1f7d6911a8743e87807992031da
Nguyen Van Linh <linh.nguyen04@hastech.vn>
2022-08-01 11:41:38 +0700

    test commit

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from models.item import Item
+from routes import fan_route
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+app.include_router(fan_route.router)
---
+++ routes/fan_route.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from typing import Union
+
+
+from models.item import Item
+
+router = APIRouter(
+    prefix="/items",
+    tags=["items"])
+
+
+@router.get("/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@router.put("/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
