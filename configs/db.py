423e23a5850f633e4e95e4cc6cbef6ec869c20d1
Nguyen Van Linh <linh.nguyen04@hastech.vn>
2022-08-04 14:34:43 +0700

    Update db.py

--- configs/db.py
+++ configs/db.py
@@ -5,8 +5,11 @@ from data.models.idol import Idol
 from configs.settings import get_settings
 
 async def initDB():
-    client = motor.motor_asyncio.AsyncIOMotorClient(get_settings().db_url)
-    await init_beanie(client[get_settings().db_name], document_models=[Idol])
+    try:
+        client = motor.motor_asyncio.AsyncIOMotorClient(get_settings().db_url)
+        await init_beanie(client[get_settings().db_name], document_models=[Idol])
+    except:
+        print("Fail to connect to DB")
 
 
 
