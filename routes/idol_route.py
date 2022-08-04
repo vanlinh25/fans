bb54c2ab5e81198b80ecdab39a5e1c16828417a4
Nguyen Van Linh <linh.nguyen04@hastech.vn>
2022-08-04 11:37:59 +0700

    Update idol_route.py

--- routes/idol_route.py
+++ routes/idol_route.py
@@ -8,10 +8,10 @@ router = APIRouter(prefix="/idols", tags=["Idols"])
 
 
 @router.get("/", response_model=list[Idol])
-async def read_item(size: Union[None, int] = 5):
+async def get_all_idols(size: Union[None, int] = 5) -> list[Idol]:
     return await Idol.find_all().to_list(size)
 
 
-@router.get("/{id}",response_model=Idol)
-async def get_idol_by_id(id: PydanticObjectId):
+@router.get("/{id}", response_model=Idol)
+async def find_idol_by_id(id: PydanticObjectId) -> Idol:
     return await Idol.get(id)
